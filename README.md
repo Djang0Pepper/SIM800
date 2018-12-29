2b978ee5783eb6cfe5b8f3152a024ffbce47e622
prehensile <me@prehensile.co.uk>
2018-12-29 21:40:29 +0000

    added skeleton pakage files

---
+++ README.md
@@ -0,0 +1,7 @@
+A library for controlling SIM800 based devices.
+
+Derived from [IteadSIM800](https://github.com/lamondlab/IteadSIM800) by [Lamond Lab](https://github.com/lamondlab).
+
+
+# TODO
+- Reimplement SMS dispatcher from origial Lamond Lab code.
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "sim800",
+    version = "0.1.0",
+    author = "Henry Cooke",
+    author_email = "me@prehensile.co.uk",
+    description = "A library for interfacing with the SIM800 GSM/GPRS Module",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/prehensile/SIM800",
+    packages = setuptools.find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 3 - Alpha",
+        "Topic :: Communications :: Telephony",
+        "Topic :: System :: Hardware"
+    ],
+)
